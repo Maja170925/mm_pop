d010c6d60b8e1adadf3dc04ff66aa3529bce27bc
Maja170925 <mmaja172005@gmail.com>
2025-06-22 22:13:27 +0200

    model danych

---
+++ utils/model.py
@@ -0,0 +1,14 @@
+toll_booth = [
+    {"name": "Punkt poboru opłat A", "location": "Olszowa"},
+    {"name": "Punkt poboru opłat B", "location": "Ostropa" },
+]
+
+employees = [
+    {"name": "Anna", "location": "Olszowa", "toll_booth": "Punkt poboru opłat A"},
+    {"name": "Tomasz", "location": "Ostropa", "toll_booth": "Punkt poboru opłat B"},
+]
+
+clients = [
+    {"name": "Adam", "location": "Olszowa", "toll_booth": "Punkt poboru opłat A"},
+    {"name": "Kasia", "location": "Ostropa" , "toll_booth": "Punkt poboru opłat B"},
+]
