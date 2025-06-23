f37c794346b9fb13a8117e4ffde454dcfa31fa0a
Maja170925 <mmaja172005@gmail.com>
2025-06-23 07:34:23 +0200

    działający kod

--- utils/model.py
+++ utils/model.py
@@ -1,14 +1,14 @@
 toll_booth = [
-    {"name": "Punkt poboru opłat A", "location": "Olszowa"},
-    {"name": "Punkt poboru opłat B", "location": "Ostropa" },
+    {"name": "Punkt poboru opłat A", "location": "Warszawa"},
+    {"name": "Punkt poboru opłat B", "location": "Poznań" },
 ]
 
 employees = [
-    {"name": "Anna", "location": "Olszowa", "toll_booth": "Punkt poboru opłat A"},
-    {"name": "Tomasz", "location": "Ostropa", "toll_booth": "Punkt poboru opłat B"},
+    {"name": "Anna", "location": "Warszawa", "toll_booth": "Punkt poboru opłat A"},
+    {"name": "Tomasz", "location": "Poznań", "toll_booth": "Punkt poboru opłat B"},
 ]
 
 clients = [
-    {"name": "Adam", "location": "Olszowa", "toll_booth": "Punkt poboru opłat A"},
-    {"name": "Kasia", "location": "Ostropa" , "toll_booth": "Punkt poboru opłat B"},
+    {"name": "Adam", "location": "Warszawa", "toll_booth": "Punkt poboru opłat A"},
+    {"name": "Kasia", "location": "Poznań" , "toll_booth": "Punkt poboru opłat B"},
 ]
